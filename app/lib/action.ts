c1ce5e6f9e1c6dd6541696db700f264ae5e9c30c
podlaskialgorytm3 <live.m851@gmail.com>
2024-03-05 21:14:04 +0100

    Fix database error handling in create, delete, and update functions

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -25,10 +25,16 @@ export async function createInvoice(formData: FormData) {
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
-  await sql`
+  try{
+    await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
+  }catch(error){
+    return {
+      message: 'Database error. Please try again.'
+    }
+  }
 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
@@ -37,7 +43,14 @@ export async function createInvoice(formData: FormData) {
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
  
 export async function deleteInvoice(id: string) {
-    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    try{
+      await sql`DELETE FROM invoices WHERE id = ${id}`;
+    }
+    catch(error){
+      return {
+        message: 'Database error. Please try again.'
+      }
+    }
     revalidatePath('/dashboard/invoices');
   } 
 
@@ -50,11 +63,18 @@ export async function updateInvoice(id: string, formData: FormData) {
  
   const amountInCents = amount * 100;
  
-  await sql`
+  try{
+    await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
+  }
+  catch(error){
+    return {
+      message: 'Database error. Please try again.'
+    }
+  }
  
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
